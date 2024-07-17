011d4dc12fc895da7d2480b050b5cebd9d1444de
Suhyun Park <suhyun_4951@naver.com>
2024-07-17 15:20:42 +0900

    설명추가

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -5,6 +5,7 @@ export class ExpenseList extends Component {
   render() {
     return (
       <React.Fragment>
+        {/* 여러 요소를 그룹화할 때 사용합니다. 불필요한 DOM 노드를 추가하지 않고 자식 요소들을 감쌀 수 있습니다. <div> 대신 사용될 수 있습니다. */}
         <ul className="list">{/* expense Item */}</ul>
         <button className="btn">목록지우기</button>
       </React.Fragment>
