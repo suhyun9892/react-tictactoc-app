eddb7eee0b293047ede38bf13e5dde1cfeb3ea10
Suhyun Park <suhyun_4951@naver.com>
2024-07-17 15:35:37 +0900

    expenseitem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,20 @@
 import React, { Component } from "react";
+import "./ExpenseItem.css";
 
 export class ExpenseItem extends Component {
   render() {
-    return <div>ExpenseItem</div>;
+    return (
+      <li className="item">
+        <div className="info">
+          <span className="expense">학원등록</span>
+          <span className="amount">1000원</span>
+        </div>
+        <div>
+          <button className="edit-btn">수정</button>
+          <button className="clear-btn">삭제</button>
+        </div>
+      </li>
+    );
   }
 }
 
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
 
 export class ExpenseList extends Component {
   render() {
     return (
       <React.Fragment>
         {/* 여러 요소를 그룹화할 때 사용합니다. 불필요한 DOM 노드를 추가하지 않고 자식 요소들을 감쌀 수 있습니다. <div> 대신 사용될 수 있습니다. */}
-        <ul className="list">{/* expense Item */}</ul>
+        <ul className="list">
+          {/* expense Item */}
+          <ExpenseItem />
+        </ul>
         <button className="btn">목록지우기</button>
       </React.Fragment>
     );
